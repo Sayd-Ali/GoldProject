5f4dff1349e494990f1764517ecac3fd594e30a1
Сайд-Али Орзамиев <obkh1309@mail.ru>
2023-08-29 17:52:23 +0300

    feat(seventh): добавление стилей и тем в проект, также добавлен провайдер для темы сайта

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 import { Routes, Route, Link } from 'react-router-dom';
 import { Suspense } from 'react';
-import { Counter } from './components';
-import './index.scss';
+import './styles/index.scss';
 import  MainPage  from "./pages/MainPage/MainPage";
 import AboutPage from "./pages/AboutPage/AboutPage";
 import { MainPageAsync } from './pages/MainPage/MainPageAsync';
 import { AboutPageAsync } from './pages/AboutPage/AboutPageAsync';
+import { useTheme } from './theme/useTheme';
 
 const App = () => {
+    const { theme, toggleTheme } = useTheme();
+
     return (
-        <div className="app">
+        <div className={`app ${theme}`}>
+            <button onClick={toggleTheme}>Toggle</button>
             <Link to={'/'}>Главная</Link>
             <Link to={'/about'}>О сайте</Link>
             <Suspense fallback={<div>Loading...</div>}>
