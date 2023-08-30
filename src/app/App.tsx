c7e52dcdfa9122936c6e7ea8ddeb70922522f862
Сайд-Али Орзамиев <obkh1309@mail.ru>
2023-08-30 21:32:13 +0300

    feat(eleventh): Добавлен AppRouter  и routeConfig для дальнейшего удобства добавления и измения страниц

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -5,6 +5,7 @@ import { useTheme } from './providers/ThemeProvider';
 import { MainPage } from 'pages/MainPage';
 import { AboutPage } from 'pages/AboutPage';
 import { classNames } from 'shared/lib';
+import { AppRouter } from './router';
 
 const App = () => {
     const { theme, toggleTheme } = useTheme();
@@ -14,12 +15,7 @@ const App = () => {
             <button onClick={toggleTheme}>Toggle</button>
             <Link to={'/'}>Главная</Link>
             <Link to={'/about'}>О сайте</Link>
-            <Suspense fallback={<div>Loading...</div>}>
-            <Routes>
-                <Route path={'/'} element={<MainPage />}/>
-                <Route path={'/about'} element={<AboutPage />}/>
-            </Routes>
-            </Suspense>
+            <AppRouter />
         </div>
     )
 }
