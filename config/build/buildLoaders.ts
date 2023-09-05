f0184e2f9a79609e4e85edaf5c440f75e5f87823
Сайд-Али Орзамиев <obkh1309@mail.ru>
2023-09-05 20:08:26 +0300

    feat(sevententh): добавлен babel  и extract plugin

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -4,6 +4,26 @@ import { BuildOptions } from "./types";
 
 export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
 
+    const babelLoader = {
+      test: /\.(js|ts|tsx)$/,
+      exclude: /node_modules/,
+      use: {
+        loader: "babel-loader",
+        options: {
+          presets: ['@babel/preset-env'],
+          "plugins": [
+            [
+              "i18next-extract", 
+              {
+                locales: ["ru", "en"],
+                keyAsDefaultValue: true
+              }
+            ]
+          ]
+        }
+      }
+    };
+
     const svgLoader = {
       test: /\.svg$/,
       use: ['@svgr/webpack'],
@@ -46,6 +66,7 @@ export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
     return [
         fileLoader,
         svgLoader,
+        babelLoader,
         typescriptLoader,
         cssLoader
     ]
